a8d0d1d5f19852fe70cb0d8644ba4545ed672088
mark-mcg <mark-mcg@users.noreply.github.com>
2022-05-15 20:50:39 +0100

    Updated to use Taxonomy.xlsx data
    Now able to generate a chart based on our real taxonomy data. Note Taxonomy.xlsx is not committed here, but the output (tree.json) is.

--- flow_data_generator.py
+++ flow_data_generator.py
@@ -13,6 +13,7 @@
 import pandas as pd
 from anytree import AnyNode, NodeMixin, RenderTree, Node, AsciiStyle, search
 from anytree.exporter import JsonExporter
+import math
 
 class FRNodeClass(NodeMixin):  # Add Node feature
     def __init__(self, name, description, id, parent=None, children=None):
@@ -28,15 +29,10 @@ class FRNodeClass(NodeMixin):  # Add Node feature
     def __str__(self):
      return "FRNode(" + self.name + ", " + self.description + ", " + str(self.id) + ")"
 
-primaryIndex = 'lifecycle'
-secondaryIndex = 'subtopic'
-leafType = "topic"
-leafDescription = "description"
-
-def main():
-    df = pd.read_excel('Test Data.xlsx', sheet_name=0)
-    #print(df)
-    root = FRNodeClass("Root", "Root", "0")
+def main(filename, sheet_name):
+    df = pd.read_excel(filename, sheet_name=sheet_name)
+    print(df)
+    root = FRNodeClass("First RespondXR", "Click the nodes to explore our map of identified vulnerabilities in the use of XR for police training.", "0")
 
     id = 1;
     # assume depth of 3 - root -> lifecycle -> subtopic -> topic
@@ -45,36 +41,37 @@ def main():
         secondaryIndexNode = None
 
         print("Parsing new row, searaching for...")
-        print(row[primaryIndex], row[secondaryIndex])
+        print(row[primaryIndex],";", row[secondaryIndex])
 
-        # insert node into tree...
-        primaryResults = search.findall(root, filter_=lambda node: node.name in (row[primaryIndex]))
-        if len(primaryResults) == 0:
-            print("primary index node not found, creating...")
-            primaryIndexNode = FRNodeClass(row[primaryIndex], "", id, root)
-            id = id+1
-            secondaryIndexNode = FRNodeClass(row[secondaryIndex], "", id, primaryIndexNode)
-            id = id+1
-        else:
-            primaryIndexNode = primaryResults[0]
-            print("primary index node found: " + str(primaryIndexNode))
-
-            # check for presence of secondary index
-            secondaryResults = search.findall(primaryIndexNode,  filter_=lambda node: node.name in (row[secondaryIndex]))
-
-            if len(secondaryResults) == 0:
-                print("secondary index node not found, creating...")
+        if isinstance(row[primaryIndex], str): # skip non-string values
+            # insert node into tree...
+            primaryResults = search.findall(root, filter_=lambda node: node.name in (row[primaryIndex]))
+            if len(primaryResults) == 0:
+                print("primary index node not found, creating...")
+                primaryIndexNode = FRNodeClass(row[primaryIndex], "", id, root)
+                id = id+1
                 secondaryIndexNode = FRNodeClass(row[secondaryIndex], "", id, primaryIndexNode)
                 id = id+1
             else:
-                secondaryIndexNode = secondaryResults[0]
-                print("secondary index node found: " + str(secondaryIndexNode))
+                primaryIndexNode = primaryResults[0]
+                print("primary index node found: " + str(primaryIndexNode))
+
+                # check for presence of secondary index
+                secondaryResults = search.findall(primaryIndexNode,  filter_=lambda node: node.name in (row[secondaryIndex]))
+
+                if len(secondaryResults) == 0:
+                    print("secondary index node not found, creating...")
+                    secondaryIndexNode = FRNodeClass(row[secondaryIndex], "", id, primaryIndexNode)
+                    id = id+1
+                else:
+                    secondaryIndexNode = secondaryResults[0]
+                    print("secondary index node found: " + str(secondaryIndexNode))
 
 
-        # should now have primary and secondary branch nodes
-        # now create our new leaf node
-        leafNode = FRNodeClass(row[leafType], row[leafDescription], id, secondaryIndexNode)
-        id = id+1
+            # should now have primary and secondary branch nodes
+            # now create our new leaf node
+            leafNode = FRNodeClass(row[leafType], row[leafDescription], id, secondaryIndexNode)
+            id = id+1
 
     print("Finished generating tree")
     # print(RenderTree(root, style=AsciiStyle()))
@@ -89,5 +86,21 @@ def main():
     f.write(jsondata)
     f.close()
 
+primaryIndex = 'lifecycle'
+secondaryIndex = 'subtopic'
+leafType = "topic"
+leafDescription = "description"
+
 if __name__ == '__main__':
-    main()
+    primaryIndex = 'lifecycle'
+    secondaryIndex = 'subtopic'
+    leafType = "topic"
+    leafDescription = "description"
+    #main('Test Data.xlsx', 0)
+
+    primaryIndex = 'Stage where harm arises'
+    secondaryIndex = 'Location of vulnerability'
+    leafType = "Vulnerability i.e. what is the specific weakness"
+    leafDescription = "Scenario description"
+    main('Taxonomy.xlsx', "Niamh - main")
+
